61af8db32002956fe6862a2660c00b4d734d0d11
elps <mar.cos11@outlook.com>
2024-08-31 09:42:58 -0300

    adicionado view de form

---
+++ app/src/main/java/br/com/fitekmaterials/ui/activity/FormMaterialActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fitekmaterials.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.fitekmaterials.R
+
+class FormMaterialActivity : AppCompatActivity(R.layout.activity_form_material) {
+
+}
--- app/src/main/java/br/com/fitekmaterials/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/fitekmaterials/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.fitekmaterials.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.fitekmaterials.R
@@ -9,11 +10,10 @@ import br.com.fitekmaterials.models.Produto
 import br.com.fitekmaterials.ui.recyclerview.adapter.ListaProdutosAdapter
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
